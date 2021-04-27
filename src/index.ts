876a27b7791e38e99fc00fd11113479b64a29ed5
toycrane <alwaysfun2183@gmail.com>
2021-04-27 14:09:47 +0900

    serverless, webpack init

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { ApolloServer } from "apollo-server-lambda";
 import { resolvers } from "./graphql/resolvers/test";
 import { typeDefs } from "./graphql/typeDefs/test";
 
-const apolloServer = new ApolloServer({ resolvers, typeDefs });
+const apolloServer = new ApolloServer({
+	resolvers,
+	typeDefs,
+});
 
 export const graphqlHandler = apolloServer.createHandler();
