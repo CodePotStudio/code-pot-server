63fff43327cb743c79158efab94f603c9a82aabf
toycrane <alwaysfun2183@gmail.com>
2021-04-29 20:21:25 +0900

    logout resolver 구현

--- src/graphql/resolvers/User/index.ts
+++ src/graphql/resolvers/User/index.ts
@@ -1,5 +1,6 @@
 import user from "./user.resolvers";
 import me from "./me.resolvers";
 import createAuthToken from "./createAuthToken.resolvers";
-const resolverArray = [user, me, createAuthToken];
+import logout from "./logout.resolvers";
+const resolverArray = [user, me, createAuthToken, logout];
 export default resolverArray;
