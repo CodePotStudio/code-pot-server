c9f41305ee75f5046b2013e1ffaeb28d0bb92cc7
toycrane <alwaysfun2183@gmail.com>
2021-04-29 20:21:00 +0900

    cookie default path 설정

--- src/plugins/customHeadersPlugin.ts
+++ src/plugins/customHeadersPlugin.ts
@@ -7,7 +7,10 @@ const customHeadersPlugin = {
 				const { setCookies = [] } = requestContext.context;
 
 				setCookies.forEach(({ name, value, options }: any) => {
-					const cookieString = cookie.serialize(name, value, options);
+					const cookieString = cookie.serialize(name, value, {
+						path: "/",
+						...options,
+					});
 					requestContext?.response?.http?.headers.set(
 						"Set-Cookie",
 						cookieString
