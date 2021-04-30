b67dee01dd86448b3653ad5e5fc9f156c5d4ec41
toycrane <alwaysfun2183@gmail.com>
2021-04-30 11:50:26 +0900

    activateUser 구현

---
+++ src/graphql/resolvers/User/activateUser.resolvers.ts
@@ -0,0 +1,26 @@
+import { Resolvers } from "../../../@types/graphql";
+
+const resolver: Resolvers = {
+	Mutation: {
+		activateUser: async (_, { mobile, name }, { prisma, user }) => {
+			if (user) {
+				const { id } = user;
+				const updatedUser = prisma.user.update({
+					where: {
+						id,
+					},
+					data: {
+						name,
+						mobile,
+						isActive: true,
+					},
+				});
+				return updatedUser;
+			} else {
+				throw new Error("User Does Not Exists");
+			}
+		},
+	},
+};
+
+export default resolver;
--- src/graphql/resolvers/User/index.ts
+++ src/graphql/resolvers/User/index.ts
@@ -3,11 +3,13 @@ import me from "./me.resolvers";
 import createAuthToken from "./createAuthToken.resolvers";
 import logout from "./logout.resolvers";
 import registerRefundAccount from "./registerRefundAccount.resolvers";
+import activateUser from "./activateUser.resolvers";
 const resolverArray = [
 	user,
 	me,
 	createAuthToken,
 	logout,
 	registerRefundAccount,
+	activateUser,
 ];
 export default resolverArray;
