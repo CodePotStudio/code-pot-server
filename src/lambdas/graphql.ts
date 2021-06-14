913584a1aeae861b0b17b3e991fda02c5e3a38c7
toycrane <alwaysfun2183@gmail.com>
2021-06-14 18:15:25 +0900

    serverless, github action 설정 변경

--- src/lambdas/graphql.ts
+++ src/lambdas/graphql.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "apollo-server-lambda";
 import ApolloConfig from "../config/apollo";
 
 const apolloServer = new ApolloServer(ApolloConfig);
-export const graphqlHandler = apolloServer.createHandler({
+export const handler = apolloServer.createHandler({
 	cors: {
 		origin: true,
 		credentials: true,
